a8791b8aab940164f34b64a126a5d3ab88158a28
Yuechen Li <yuechenli@outlook.com>
2026-02-21 14:01:02 -0800

    Add project files.

---
+++ src/Dominatus.Core/StateId.cs
@@ -0,0 +1,7 @@
+﻿namespace Dominatus.Core;
+
+public readonly record struct StateId(string Value)
+{
+    public override string ToString() => Value;
+    public static implicit operator StateId(string s) => new(s);
+}
---
+++ src/Dominatus.OptFlow/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Dominatus.OptFlow
+{
+    public class Class1
+    {
+
+    }
+}
